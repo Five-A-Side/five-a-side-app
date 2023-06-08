078f054245b56ede6f169336b7253550605836d5
João Cardoso <joao.fonsecacardoso@gmail.com>
2023-06-08 14:18:05 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/five_a_side_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.five_a_side_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/config/network/endpoints.dart
@@ -0,0 +1,4 @@
+class EndPoints {
+  static const String baseUrl = 'http://localhost:3000';
+  static const String sampleEndpoint = '/samples/endpoint';
+}
